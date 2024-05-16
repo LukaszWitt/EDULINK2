b3588a2a01e54de57bebc4a1706ce3a924f2eee9
LukaszWitt <witt.config@gmail.com>
2024-05-16 12:04:24 +0200

    zrobione logowanie

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 
 import { Box } from "@mui/material";
-import Background from "@/components/Background";
-import m from '../public/images/grafika2.webp'
+
 
 export default function Home() {
   return (
